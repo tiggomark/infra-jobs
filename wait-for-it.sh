41e4a9666ba8719b45ff393eacaf4728a7414f47
thomazrd <thomaz.damasceno-ext@via.com.br>
2022-07-26 11:36:43 -0300

    Initial

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -29,9 +29,9 @@ fi
 # Begin script in case all parameters are correct
 
 
-while [[ ${STATUS_RECEIVED} != ${status} ]];
-        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${host}) &&
-        echo "received status: $STATUS_RECEIVED" &&
+while [[ ${STATUS_RECEIVED} != ${status} ]]; do
+        STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${host});
+        echo "received status: $STATUS_RECEIVED";
         sleep 1;
     done;
     echo success with status: $STATUS_RECEIVED'
