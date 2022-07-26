4b0ba4da48de6f9ad76da7b5e91dc4c572ed3ac1
thomazrd <thomaz.damasceno-ext@via.com.br>
2022-07-26 11:18:25 -0300

    Initial

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,6 +1,34 @@
+
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -a parameterA -b parameterB -c parameterC"
+   echo -e "\t-a Description of what is parameterA"
+   echo -e "\t-b Description of what is parameterB"
+   echo -e "\t-c Description of what is parameterC"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "h:" opt
+do
+   case "$opt" in
+      h ) host="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$host" ] || [ -z "$host" ]]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
 curl --connect-timeout 5 \
     --max-time 10 \
     --retry 5 \
     --retry-delay 0 \
     --retry-max-time 40 \
-    'http://www.google.com'
+    $host
