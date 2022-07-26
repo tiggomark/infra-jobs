ba33216d4f927ac6835c1fbde2eb43bfb73d7d47
thomazrd <thomaz.damasceno-ext@via.com.br>
2022-07-26 11:09:40 -0300

    Initial

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -5,10 +5,6 @@ declare HOST=$1
 declare STATUS=$2
 declare TIMEOUT=$3
 
-HOST=$HOST STATUS=$STATUS timeout --foreground -s TERM $TIMEOUT bash -c \
-    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
-        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
-        echo "received status: $STATUS_RECEIVED" && \
-        sleep 1;\
-    done;
-    echo success with status: $STATUS_RECEIVED'
+
+
+curl --retry 10 --retry-delay 5 -s -o /dev/null $HOST
