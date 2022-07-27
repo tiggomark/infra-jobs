199ee2989d9c7c94d7554b17f05929d4b2961670
thomazrd <thomaz.damasceno-ext@via.com.br>
2022-07-27 17:59:42 -0300

    Adicionado Portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,4 @@
+docker run -d -p 9000:9000  --name portainer -v "//var/run/docker.sock:/var/run/docker.sock" portainer/portainer
+
+docker exec -u root -it portainer /bin/bash
+chmod 666 /var/run/docker.sock
